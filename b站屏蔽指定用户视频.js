26c90838fe53e8c21f2e8963977942810df5572e
byhgz <79256242+hgztask@users.noreply.github.com>
2023-03-03 14:26:06 +0800

    Add files via upload

---
+++ b站屏蔽指定用户视频.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         b站屏蔽指定用户频道排行榜视频
+// @version      0.1
+// @description  屏蔽场景，频道的排行榜读音用户视频
+// @author       byhgz
+// @match        https://www.bilibili.com/v/channel/*?tab=multiple
+// @icon         https://static.hdslb.com/images/favicon.ico
+// @require  https://unpkg.com/ajax-hook@2.1.3/dist/ajaxhook.min.js
+// @grant        none
+// ==/UserScript==
+
+/**
+ * 用户名黑名单模式
+ * @type {string[]}
+ */
+const arr = ["疯狂暗示00", "silly沐"];
+/**
+ * 用户uid黑名单模式
+ * @type {number[]}
+ */
+const arrid = [3493087556930157];
+
+/**
+ * 隐藏对应元素的视频
+ * @param vdoc 视频列表元素
+ * @param arr 用户名数组
+ * @param arrid 用户id数组
+ */
+function startExtracted(vdoc, arr, arrid) {
+  for (const element of vdoc) {
+    //用户名
+    const uPameText = element.getElementsByClassName("up-name__text")[0].textContent;
+    let indexOf = arr.indexOf(uPameText);
+    //视频标题
+    let videoName = element.getElementsByClassName("video-name")[0].textContent;
+    console.log(uPameText)
+    if (indexOf !== -1) {
+      element.style.display = "none";
+      console.log("已通过用户名=【" + uPameText + "】屏蔽指定黑名单用户视频=" + videoName);
+      continue;
+    }
+    //空间地址
+    const upSpatialAddress = element.getElementsByClassName("up-name")[0].getAttribute("href");
+    const lastIndexOf = upSpatialAddress.lastIndexOf("/") + 1;
+    const id = parseInt(upSpatialAddress.substring(lastIndexOf));
+    if (arrid.includes(id)) {
+      element.style.display = "none";
+      console.log("已通过id=【" + id +"】屏蔽指定黑名单用户【"+ uPameText+"】视频="+ uPameText);
+    }
+  }
+}
+
+
+/**
+ * 针对综合排行榜和频道中的其他精选视频以及综合排行榜下面的视频
+ * @param arr
+ * @param arrid
+ */
+function extracted(arr, arrid) {
+  //针对综合排行榜
+  const comprehensiveRanking = document.getElementsByClassName("rank-video-card");
+  startExtracted(comprehensiveRanking, arr, arrid);
+  //针对频道中的其他精选视频和在综合排行榜下面的视频
+  const cardList = document.getElementsByClassName("card-list");
+  for (let v of cardList) {
+    const temp = v.getElementsByClassName("video-card");
+    startExtracted(temp, arr, arrid);
+  }
+}
+
+(function () {
+  'use strict';
+  extracted(arr, arrid);
+  ah.proxy({
+    // //请求发起前进入
+    onRequest: (config, handler) => {
+      handler.next(config);
+    },
+    //请求发生错误时进入，比如超时；注意，不包括http状态码错误，如404仍然会认为请求成功
+    onError: (err, handler) => {
+      handler.next(err);
+    },
+    //请求成功后进入
+    onResponse: (response, handler) => {
+      const responseURL = handler.xhr.responseURL;
+      handler.next(response);
+      if (responseURL.includes("https://api.bilibili.com/x/web-interface/web/channel/multipl")) {
+        console.log("检测到符合要求的请求=" + responseURL);
+        extracted(arr, arrid);
+      }
+    }
+  });
+
+  // Your code here...
+})();
