17818ec04db0c9e8bcacd176706bdfc6f6f6a02b
byhgz <byhgz@qq.com>
2023-05-14 16:56:54 +0800

    更新版本和信息

--- BIBIShield.js
+++ BIBIShield.js
@@ -2,7 +2,7 @@
 // @name         b站屏蔽增强器
 // @namespace    http://tampermonkey.net/
 // @license      MIT
-// @version      1.1.45
+// @version      1.1.46
 // @description  根据用户名、uid、视频关键词、言论关键词和视频时长进行屏蔽和精简处理(详情看脚本主页描述)，针对github站内所有的链接都从新的标签页打开，而不从当前页面打开
 // @author       byhgz
 // @exclude      *://message.bilibili.com/pages/nav/header_sync
