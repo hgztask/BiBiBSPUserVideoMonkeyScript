062b5ad34aa430c8e8e7d0552caf8d0d54c32202
寒鸽子 <byhgz@qq.com>
2023-06-06 21:06:25 +0800

    更新描述

--- other/油猴本地开发元规则.js
+++ other/油猴本地开发元规则.js
@@ -6,8 +6,16 @@
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\AccountCenter.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\FrequencyChannel.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\Live.js
+// @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\Trends.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\Bilibili.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\BilibiliOne.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\RuleList.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\other\Github.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\BIBIShield.js
+
+
+/**
+
+
+
+ */
