5a7cd2e6aca9fd43ad777cb5a69568319dc028d4
寒鸽子 <byhgz@qq.com>
2023-06-07 21:12:59 +0800

    获取本地格式化规则函数去除不需要的相关规则

--- util/Util.js
+++ util/Util.js
@@ -356,7 +356,7 @@ const Util = {
     "标题黑名单模式(模糊匹配)": ${JSON.stringify(LocalData.getArrTitle())},"标题黑名单模式(正则匹配)": ${JSON.stringify(LocalData.getArrTitleKeyCanonical())},
     "评论关键词黑名单模式(模糊匹配)": ${JSON.stringify(Util.getData("commentOnKeyArr"))},"评论关键词黑名单模式(正则匹配)": ${JSON.stringify(LocalData.getArrContentOnKeyCanonicalArr())},
     "粉丝牌黑名单模式(精确匹配)": ${JSON.stringify(LocalData.getFanCardArr())},"专栏关键词内容黑名单模式(模糊匹配)": ${JSON.stringify(LocalData.getContentColumnKeyArr())},
-    "动态关键词内容黑名单模式(模糊匹配)": ${JSON.stringify(LocalData.getDynamicArr())},"禁用快捷悬浮屏蔽面板自动显示":${Util.getData("isDShielPanel")}}`;
+    "动态关键词内容黑名单模式(模糊匹配)": ${JSON.stringify(LocalData.getDynamicArr())}}`;
     },
     /**
      * 设置页面播放器的播放速度
