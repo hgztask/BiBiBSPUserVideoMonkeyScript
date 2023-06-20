9f8cf28678ee792c0ea29fd2131d525058444400
寒鸽子 <byhgz@qq.com>
2023-06-20 22:45:13 +0800

    新增Space文件，该主要是处理个人控件中的相关逻辑，目前大部分代码是配合后续新增的后去用户粉丝或者关注列表逻辑配套使用

---
+++ code/Space.js
@@ -0,0 +1,90 @@
+const Space = {
+    isFetchingFollowersOrWatchlists: false,//是否正在获取粉丝或关注列表
+    isSpaceFollowOrFollow: function (url) {//判断url最后的地址是否是关注或粉丝参数
+        let type;
+        const match = /\/fans\/(.*?)\?/.exec(url);
+        const split = url.split("/");
+        if (match && match[1]) {
+            type = match[1];
+        } else {
+            type = split[split.length - 1];
+        }
+        switch (type) {
+            case "follow"://关注数
+            case "fans"://粉丝
+                return type;
+            default:
+                return null;
+        }
+    },
+    getRealtIonList: function () {
+        return new Promise((resolve, reject) => {
+            const interval = setInterval(() => {
+                const list = document.querySelectorAll("ul[class='relation-list']>*"); //获取每个项目
+                if (list.length === 0) {
+                    reject(null);
+                    return;
+                }
+                clearInterval(interval);
+                const userinfoList = [];
+                list.forEach(value => {
+                    const userInfo = {};
+                    const userInfoContent = value.querySelector(".content");
+                    const userAddress = userInfoContent.querySelector("a").getAttribute("href");
+                    const name = userInfoContent.querySelector("a>.fans-name").textContent;
+                    let desc = userInfoContent.querySelector(".desc");//个人简介
+                    const fansActionText = userInfoContent.querySelector(".fans-action-text").textContent;//关注状态，如已关注，互关
+                    const userImg = value.querySelector(".cover-container .bili-avatar>img").getAttribute("src");//头像
+                    userInfo["name"] = name;
+                    userInfo["img"] = userImg;
+                    if (desc !== null) {
+                        desc = desc.getAttribute("title");
+                    }
+                    userInfo["desc"] = desc;
+                    userInfo["address"] = userAddress;
+                    userInfo["fansActionType"] = fansActionText;
+                    userInfo["e"] = value;
+                    userinfoList.push(userInfo);
+                });
+                resolve(userinfoList);
+            }, 1000);
+        });
+    }
+    ,
+    extracted: function (loading) {
+        return new Promise(resolve => {
+            let dataList = [];
+
+            function whileFunc() {
+                Space.getRealtIonList().then(value => {
+                    const currentInfo = `当前列表获取个数：${value.length}`;
+                    Qmsg.success(currentInfo);
+                    console.log(currentInfo);
+                    const next = $(".be-pager-next");
+                    dataList = dataList.concat(value);
+                    Qmsg.success(`总列表个数：${dataList.length}`);
+                    if (next.is(':hidden')) {//判断是否已经隐藏了下一页，隐藏说明没有下一页的关注列了，到头了，反之还有
+                        loading.close();
+                        resolve(dataList);
+                        return;
+                    }
+                    next.click();
+                    console.log("点击下一页");
+                    Qmsg.info("点击下一页");
+                    setTimeout(() => {
+                        whileFunc();
+                    }, 1000);
+                }).catch(() => {
+                    loading.close();
+                    if (dataList.length === 0) {
+                        Qmsg.error("获取失败");
+                        return;
+                    }
+                    resolve(dataList);
+                });
+            }
+
+            whileFunc();
+        });
+    }
+}
