62eef14441c16d199069d5967faf6d32717eeb23
寒鸽子 <byhgz@qq.com>
2023-06-20 22:43:04 +0800

    移除RuleList.js并该该文件的代码移动至bilibili.js文件

--- code/other/油猴本地开发元规则.js
+++ code/other/油猴本地开发元规则.js
@@ -8,9 +8,9 @@
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\FrequencyChannel.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\Live.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\Trends.js
+// @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\Space.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\Bilibili.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\BilibiliOne.js
-// @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\RuleList.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\other\Github.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\other\GBTGame.js
 // @require      file:///D:\DataFile\other\idea\project\BiBiBSPUserVideoMonkeyScript\code\main.js
