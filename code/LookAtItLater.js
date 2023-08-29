2322175d688ee885209d50fc82b8a026e9cf22c2
寒鸽子 <byhgz@qq.com>
2023-08-29 23:59:09 +0800

    实现addLookAtItLater添加视频到稍后再看列表流程函数

--- code/LookAtItLater.js
+++ code/LookAtItLater.js
@@ -6,5 +6,22 @@ const LookAtItLater = {
                 lookAtItLaterList: LocalData.getLookAtItLaterArr()
             }
         })
+    },
+    addLookAtItLater(data) {//添加视频到稍后再看列表流程
+        if (!confirm(`是要将【${data["title"]}】添加进稍后再看列表吗？`)) {
+            return;
+        }
+        const arr = LocalData.getLookAtItLaterArr();
+        for (const v of arr) {
+            const tempTitle = data["title"];
+            if (v["title"] === tempTitle) {
+                alert(`您已添加该视频【${tempTitle}】！故本轮不添加进去！`);
+                return;
+            }
+        }
+        arr.push(data);
+        LocalData.setLookAtItLaterArr(arr);
+        Qmsg.success("添加成功！")
+        alert(`已添加视频【${data["title"]}】至稍后再看列表！`);
     }
 }
