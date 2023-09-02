87ee54324d17d8f07227ca49024a475b46a566a1
寒鸽子 <byhgz@qq.com>
2023-09-03 00:28:41 +0800

    新增相关set方法并链式方式调用

--- code/Class.js
+++ code/Class.js
@@ -1,19 +1,70 @@
+/**
+ * 用户基本信息
+ */
 class userClass {
     upName;
     uid;
+    upAddress;
+
+    setUpName(upName) {
+        this.upName = upName;
+        return this;
+    }
+
+    setUpAddress(upAddress) {
+        this.upAddress = upAddress;
+        return this;
+    }
+
+    setUid(uid) {
+        this.uid = uid;
+        return this;
+    }
 }
 
+/**
+ * 视频基本信息
+ */
 class VideoClass extends userClass {
     title;
     bv;
+    av;
     videoAddress;
-    upAddress;
+    videoTime;
+    playbackVolume;
 
-    constructor(title, bv, videoAddress, upAddress) {
-        super();
+    setTitle(title) {
         this.title = title;
-        this.bv = bv;
-        this.videoAddress = videoAddress;
-        this.upAddress = upAddress;
+        return this;
     }
+
+    setBv(bv) {
+        this.bv = bv;
+        return this;
+    }
+
+    setAv(av) {
+        this.av = av;
+        return this;
+    }
+
+    setVideoAddress(videoAddress) {//设置视频地址
+        this.videoAddress = videoAddress;
+        return this;
+    }
+
+    setVideoTime(videoTime) {//设置时长
+        this.videoTime = videoTime;
+        return this;
+    }
+
+    //设置播放量
+    setPlaybackVolume(playbackVolume) {
+        this.playbackVolume = playbackVolume;
+        return this;
+    }
+
 }
+
+
+
