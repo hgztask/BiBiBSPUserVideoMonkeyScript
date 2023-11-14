e0ecdc9e5692210f8dcb8c09d733129409357d4b
寒鸽子 <byhgz@qq.com>
2023-11-14 14:12:57 +0800

    调整了注册地址

--- code/other/Api.js
+++ code/other/Api.js
@@ -1,5 +1,5 @@
 const defApi = "https://api.mikuchase.ltd";
-const registeredAddress = "https://www.mikuchase.ltd/web/#/register";
+const registeredAddress = "https://www.mikuchase.ltd/web/#/registerAndLogIn";
 /**
  * 踩坑记录，如果在ajax中使用地址时，后面记得加/
  */
