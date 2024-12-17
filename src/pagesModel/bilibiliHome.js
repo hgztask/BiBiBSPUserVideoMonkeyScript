3b7e6e06fdc0d1265d29150df239eea995354271
寒鸽子 <2978778354@qq.com>
2024-12-18 01:51:07 +0800

    适配Bilibili-Gate脚本针对于首页视频列表的修改，原bilibili-app-recommend脚本

--- src/pagesModel/bilibiliHome.js
+++ src/pagesModel/bilibiliHome.js
@@ -2,6 +2,7 @@ import elUtil from "../utils/elUtil.js";
 import shielding from "../model/shielding.js";
 import {Tip} from "../utils/Tip.js";
 import defUtil from "../utils/defUtil.js";
+import localMKData from "../data/localMKData.js";
 
 
 // 判断是否是首页
@@ -14,6 +15,9 @@ const isHome = (url, title) => {
     }
     return url === 'https://www.bilibili.com/?spm_id_from=333.1007.0.0'
 }
+
+// 是否适配BAppcommerce脚本
+const adaptationBAppCommerce = localMKData.getAdaptationBAppCommerce();
 
 
 // 删除下载提示
@@ -105,9 +109,71 @@ const getHomeVideoELList = async () => {
 
 
 /**
+ * 获取Bilibili-Gate脚本下的首页其脚本自带的激活的选项卡
+ * @returns {Promise<string>}
+ */
+const getGateActivatedTab = async () => {
+    const el = await elUtil.findElementUntilFound(".ant-radio-group>.ant-radio-button-wrapper-checked .css-1k4kcw8")
+    return el?.textContent.trim();
+}
+
+
+/**
+ * 获取Bilibili-Gate脚本下的首页视频列表
+ * @returns {Promise<[{}]>}
+ */
+const getGateDataList = async () => {
+    const elList = await elUtil.findElementsUntilFound(".bilibili-gate-video-grid>[data-bvid].bili-video-card")
+    const list = [];
+    for (let el of elList) {
+        const videoUrl = el.querySelector("a.css-feo88y").href;
+        const name = el.querySelector(".bili-video-card__info--author").textContent.trim();
+        const title = el.querySelector(".bili-video-card__info--tit").textContent.trim();
+        const insertionPositionEl = el.querySelector(".bili-video-card__info--owner");
+        const userUrl = insertionPositionEl.href;
+        const uid = elUtil.getUrlUID(userUrl);
+        list.push({
+            title,
+            userUrl,
+            name,
+            uid,
+            videoUrl,
+            el,
+            insertionPositionEl,
+            explicitSubjectEl: el
+        })
+    }
+    return list;
+}
+
+const startShieldingGateVideoList = async () => {
+    const list = await getGateDataList()
+    for (let videoData of list) {
+        if (shielding.shieldingVideoDecorated(videoData,"hide")) {
+            continue;
+        }
+        shielding.addVideoBlockButton({data: videoData, maskingFunc: startShieldingGateVideoList});
+    }
+}
+
+/**
+ * 每秒检测是否需要屏蔽首页中视频列表(适配BAppcommerce脚本)
+ */
+const startIntervalShieldingGateVideoList = () => {
+    const throttle = defUtil.throttle(startShieldingGateVideoList, 2000);
+    setInterval(async () => {
+        const tabName = await getGateActivatedTab();
+        throttle();
+    }, 1500);
+}
+
+
+/**
  * 每秒检测清理首页中视频列表里多余的内容，如直播，番剧推荐等
  */
 const startClearExcessContentList = () => {
+    //如果开启适配BAppcommerce脚本，则不执行此功能
+    if (adaptationBAppCommerce) return;
     //每秒清理一次
     setInterval(() => {
         const otherElList = document.querySelectorAll(".floor-single-card");
@@ -141,7 +207,7 @@ const startDebounceShieldingChangeVideoList = defUtil.debounce(startShieldingCha
 //开始屏蔽首页中换一换下面的视频列表
 const startShieldingHomeVideoList = async () => {
     const homeVideoELList = await getHomeVideoELList();
-    for (let videoData of homeVideoELList) {
+    for (const videoData of homeVideoELList) {
         if (shielding.shieldingVideoDecorated(videoData)) {
             continue;
         }
@@ -160,6 +226,8 @@ const startDebounceShieldingHomeVideoList = defUtil.debounce(startShieldingHomeV
  * @returns {Promise<>}
  */
 const scrollMouseUpAndDown = async () => {
+    //如果开启适配BAppcommerce脚本，则不执行此功能
+    if (adaptationBAppCommerce) return;
     await defUtil.smoothScroll(false, 100);
     return defUtil.smoothScroll(true, 600);
 }
@@ -171,5 +239,6 @@ export default {
     startDebounceShieldingChangeVideoList,
     startDebounceShieldingHomeVideoList,
     scrollMouseUpAndDown,
-    deDesktopDownloadTipEl
+    deDesktopDownloadTipEl,
+    startIntervalShieldingGateVideoList
 }
