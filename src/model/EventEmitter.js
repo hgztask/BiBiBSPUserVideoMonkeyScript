29ea7a02b7f3ca9a699c99b395f48a2034d39d0a
寒鸽子 <2978778354@qq.com>
2025-01-25 22:36:32 +0800

    refactor(model): 重构 EventEmitter 类
    - 重命名私有字段 #ordinaryEvents 为 #regularEvents，提高代码可读性
    - 重命名 #handlerEvent 为 #callbackEvents，更准确地反映其功能
    - 优化了事件订阅、发送和移除等方法的实现，提高代码性能和可维护性
    - 更新了方法注释，增加了参数类型注解，提高了代码文档质量

--- src/model/EventEmitter.js
+++ src/model/EventEmitter.js
@@ -1,11 +1,12 @@
 /**
  * 事件中心类，用于管理事件的订阅和发布。
+ * 提供了订阅普通事件、一次性订阅普通事件、订阅回调事件、发送通知、发送普通消息等功能。
  */
 class EventEmitter {
     /**
-     * 普通事件中心
+     * 普通事件中心，存储已订阅的普通事件和待订阅的普通事件。
      */
-    #ordinaryEvents = {
+    #regularEvents = {
         // 普通事件
         events: {},
         // 待订阅事件
@@ -13,14 +14,14 @@ class EventEmitter {
     }
 
     /**
-     * 回调事件中心，接收并处理消息。发送、接受、返回结果
-     * @type {{events:{},invoke_interval:number}}
+     * 回调事件中心，接收并处理消息。发送、接受、返回结果。
+     * @type {{events:{}, callbackInterval:number}}
      */
-    #handlerEvent = {
+    #callbackEvents = {
         // 回调事件
         events: {},
         // 回调事件查找事件间隔时间
-        invoke_interval: 1500
+        callbackInterval: 1500
     }
 
     /**
@@ -32,7 +33,7 @@ class EventEmitter {
      */
     on(eventName, callback) {
         // 处理已订阅事件
-        const events = this.#ordinaryEvents.events;
+        const events = this.#regularEvents.events;
         if (events[eventName]) {
             // 如果事件已经订阅，则将回调函数添加到事件监听器表中
             events[eventName].push(callback);
@@ -42,7 +43,7 @@ class EventEmitter {
         events[eventName] = []
         events[eventName].push(callback);
         //处理待订阅事件
-        const futureEvents = this.#ordinaryEvents.futures;
+        const futureEvents = this.#regularEvents.futures;
         if (futureEvents[eventName]) {
             //如果待订阅事件中有该事件名，则依次执行该事件名对应的回调函数
             for (const futureEvent of futureEvents[eventName]) {
@@ -53,26 +54,26 @@ class EventEmitter {
     }
 
     /**
-     * 一次性订阅普通事件，将回调函数添加到事件监听器表中，只执行一次，执行完移除对应的回调函数
+     * 一次性订阅普通事件，将回调函数添加到事件监听器表中，只执行一次，执行完移除对应的回调函数。
      * @param {string} eventName - 事件名称
      * @param {function} callback - 一次性回调函数，事件触发后自动取消订阅对应函数
      */
     once(eventName, callback) {
         const onceCallback = (...args) => {
             callback(...args);
-            this.#off(eventName, onceCallback);
+            this.#removeCallback(eventName, onceCallback);
         };
         this.on(eventName, onceCallback);
     }
 
     /**
      * 订阅回调事件，接收并处理消息。当接收到特定的消息时，执行该处理函数，并将结果返回发送方。
-     * 回调事件只能一个对应的回调函数，如需覆盖，则需要重新订阅
-     * @param eventName {string} 事件名
-     * @param callback {function}
+     * 回调事件只能有一个对应的回调函数，如需覆盖，则需要重新订阅。
+     * @param {string} eventName - 事件名称
+     * @param {function} callback - 处理消息的回调函数
      */
     handler(eventName, callback) {
-        const handlerEvents = this.#handlerEvent.events;
+        const handlerEvents = this.#callbackEvents.events;
         if (handlerEvents[eventName]) {
             throw new Error('该事件名已经存在，请更换事件名')
         }
@@ -80,14 +81,14 @@ class EventEmitter {
     }
 
     /**
-     * 发送通知，如未订阅，则一直等到回调订阅事件存在，拿到结果之后返回结果
-     * @param eventName {string} 事件名
-     * @param data {...*}数据
-     * @returns {Promise<any|boolean|string|any|Set|Map|Element|Document>} 返回结果
+     * 发送通知，如未订阅，则一直等到回调订阅事件存在，拿到结果之后返回结果。
+     * @param {string} eventName - 事件名称
+     * @param {...*} data - 发送的数据
+     * @returns {Promise<any>} 返回处理结果
      */
     invoke(eventName, ...data) {
         return new Promise(resolve => {
-            const handlerEvents = this.#handlerEvent.events;
+            const handlerEvents = this.#callbackEvents.events;
             if (handlerEvents[eventName]) {
                 resolve(handlerEvents[eventName](...data));
                 return
@@ -97,18 +98,18 @@ class EventEmitter {
                     clearInterval(i1)
                     resolve(handlerEvents[eventName](...data));
                 }
-            }, this.#handlerEvent.invoke_interval);
+            }, this.#callbackEvents.callbackInterval);
         })
     }
 
 
     /**
-     *发送普通消息，如未订阅事件，直到订阅到事件时发送
-     * @param eventName {string} 事件类型
-     * @param data {...*} 事件数据，可多个参
+     *发送普通消息，如未订阅事件，直到订阅到事件时发送。
+     * @param {string} eventName - 事件名称
+     * @param {...*} data - 发送的数据
      */
     send(eventName, ...data) {
-        const ordinaryEvents = this.#ordinaryEvents;
+        const ordinaryEvents = this.#regularEvents;
         const events = ordinaryEvents.events;
         const event = events[eventName];
         if (event) {
@@ -129,35 +130,35 @@ class EventEmitter {
     }
 
     /**
-     * 私有取消订阅事件，用于内部临时使用
-     * 事件名称会保留，当对应的事件函数会被移除
+     * 私有取消订阅事件，用于内部临时使用。
+     * 事件名称会保留，当对应的事件函数会被移除。
      * @param {string} eventName - 事件名称
      * @param {function} callback - 要取消的回调函数
      */
-    #off(eventName, callback) {
-        const events = this.#ordinaryEvents.events;
+    #removeCallback(eventName, callback) {
+        const events = this.#regularEvents.events;
         if (events[eventName]) {
             events[eventName] = events[eventName].filter(cb => cb !== callback);
         }
-        const handlerEvents = this.#handlerEvent.events;
+        const handlerEvents = this.#callbackEvents.events;
         if (handlerEvents[eventName]) {
             handlerEvents[eventName] = handlerEvents[eventName].filter(cb => cb !== callback);
         }
     }
 
     /**
-     * 移除对应事件名的订阅
-     * 会在事件中心中移除对应事件名和对应的事件函数
-     * @param eventName {string} 要移除的事件名
+     * 移除对应事件名的订阅。
+     * 会在事件中心中移除对应事件名和对应的事件函数。
+     * @param {string} eventName - 要移除的事件名
      * @returns {boolean} 是否移除成功
      */
     off(eventName) {
-        const events = this.#ordinaryEvents.events;
+        const events = this.#regularEvents.events;
         if (events[eventName]) {
             delete events[eventName]
             return true
         }
-        const handlerEvents = this.#handlerEvent.events;
+        const handlerEvents = this.#callbackEvents.events;
         if (handlerEvents[eventName]) {
             delete handlerEvents[eventName]
             return true
@@ -167,11 +168,11 @@ class EventEmitter {
 
 
     /**
-     * 设置回调事件查找事件间隔时间
-     * @param interval {number} 间隔时间，单位为毫秒
+     * 设置回调事件查找事件间隔时间。
+     * @param {number} interval - 间隔时间，单位为毫秒
      */
     setInvokeInterval(interval) {
-        this.#handlerEvent.invoke_interval = interval;
+        this.#callbackEvents.callbackInterval = interval;
     }
 }
 
