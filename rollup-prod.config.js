d4d80ae09e98f619f8286e2c01e6b9c94513141f
寒鸽子 <2978778354@qq.com>
2025-02-04 22:57:06 +0800

    build:优化项目构建配置- 新增 rollup-prod.config.js 配置文件，用于生产环境构建 - 更新 package.json脚本，增加生产环境构建命令 - 修改 rollup.config.js，移除数组包装，简化配置结构

--- rollup-prod.config.js
+++ rollup-prod.config.js
@@ -1,4 +1,5 @@
 import importContent from 'rollup-plugin-import-content'
+import {terser} from "rollup-plugin-terser";
 
 export default {
     input: 'src/main.js',
@@ -7,15 +8,17 @@ export default {
         importContent({
             fileName: ['.css']
         }),
-        // terser({
-        //     compress: {
-        //         drop_console: false, // 不删除 console.log 语句
-        //         drop_debugger: false // 不删除 debugger 语句
-        //     }
-        // })
+        terser({
+            compress: {
+                // 是否删除 console.log
+                drop_console: false,
+                // 是否删除 debugger
+                drop_debugger: true
+            }
+        })
     ],
     output: {
-        file: 'dist/local_build.js',
+        file: 'dist/local-prod-build.js',
         format: 'iife',
         globals: {
             vue: "Vue", // 这里指定 'vue' 模块对应的全局变量名为 'Vue'
