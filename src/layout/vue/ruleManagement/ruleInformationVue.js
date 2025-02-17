ad7d579de7be4e93c0c8655fd0976f57cd9c7f2a
寒鸽子 <2978778354@qq.com>
2025-02-18 00:01:17 +0800

    refactor(ruleManagement): 重构规则信息展示界面
    - 使用 Element UI 组件替换原始的 HTML 结构
    - 用 el-button 替换原始按钮
    - 使用 el-descriptions 和 el-descriptions-item 组件展示规则信息
    - 添加 el-badge 组件显示规则数量

--- src/layout/vue/ruleManagement/ruleInformationVue.js
+++ src/layout/vue/ruleManagement/ruleInformationVue.js
@@ -8,12 +8,15 @@ import ruleUtil from "../../../utils/ruleUtil.js";
 export default {
     template: `
       <div>
-      <h2>规则信息</h2>
-      <button gz_type @click="refreshInfoBut">刷新信息</button>
-      <div v-for="item in ruleInfoArr"
-           style="padding: 5px">
-        {{ item.name }}{{ item.len }}个
-      </div>
+        <el-button @click="refreshInfoBut">刷新信息</el-button>
+        <el-descriptions title="规则信息">
+          <el-descriptions-item v-for="item in ruleInfoArr" :key="item.name"
+                                :label="item.name">
+            <el-badge :value="item.len">
+              <el-button>个</el-button>
+            </el-badge>
+          </el-descriptions-item>
+        </el-descriptions>
       </div>`,
     data() {
         return {
