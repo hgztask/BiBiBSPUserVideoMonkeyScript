6246abd6ad7e6414df5516c37a881370626646d3
寒鸽子 <2978778354@qq.com>
2025-02-24 00:08:08 +0800

    refactor(layout): 优化高级规则页面布局
    - 移除了多余的 el-card 组件
    -调整了 el-row 和 el-col 组件的使用，使布局更加合理
    - 删除了未使用的 div 元素
    - 统一了卡片样式的阴影效果

--- src/layout/vue/ruleManagement/highLevelRuleVue.js
+++ src/layout/vue/ruleManagement/highLevelRuleVue.js
@@ -22,39 +22,36 @@ export const high_level_rule_vue = {
           <el-switch v-model="is_vertical_val" active-text="屏蔽竖屏类视频"/>
           <el-switch v-model="blockFollowed" active-text="屏蔽已关注"/>
           <el-switch v-model="is_up_owner_exclusive" active-text="屏蔽充电专属视频"></el-switch>
+          <el-switch v-model="is_senior_member_val" active-text="屏蔽硬核会员"/>
+          <el-row>
+            <el-col :span="12">
+              <el-card shadow="never">
+                <template #header>会员类型屏蔽</template>
+                <el-radio-group v-model="vipTypeRadioVal">
+                  <el-radio-button label="无"></el-radio-button>
+                  <el-radio-button label="月大会员"></el-radio-button>
+                  <el-radio-button label="年度及以上大会员"></el-radio-button>
+                  <el-radio-button label="不处理"></el-radio-button>
+                </el-radio-group>
+              </el-card>
+            </el-col>
+            <el-col :span="12">
+              <el-card shadow="never">
+                <template #header>性别屏蔽</template>
+                <el-radio-group v-model="genderRadioVal">
+                  <el-radio-button label="男性"></el-radio-button>
+                  <el-radio-button label="女性"></el-radio-button>
+                  <el-radio-button label="保密"></el-radio-button>
+                  <el-radio-button label="不处理"></el-radio-button>
+                </el-radio-group>
+              </el-card>
+            </el-col>
+          </el-row>
         </el-card>
-        <el-row>
-          <el-col :span="12">
-            <el-card>
-              <template #header>会员类型屏蔽</template>
-              <el-radio-group v-model="vipTypeRadioVal">
-                <el-radio-button label="无"></el-radio-button>
-                <el-radio-button label="月大会员"></el-radio-button>
-                <el-radio-button label="年度及以上大会员"></el-radio-button>
-                <el-radio-button label="不处理"></el-radio-button>
-              </el-radio-group>
-            </el-card>
-          </el-col>
-          <el-col :span="12">
-            <el-card>
-              <template #header>是否屏蔽硬核会员</template>
-              <el-switch v-model="is_senior_member_val" active-text="是" inactive-text="否"/>
-            </el-card>
-          </el-col>
-        </el-row>
         <el-card>
           <template #header>计算创作团队</template>
           <el-tooltip content="当作者未匹配上时检查其他成员"></el-tooltip>
           <el-switch v-model="is_check_team_member" active-text="检查创作团队中成员"/>
-        </el-card>
-        <el-card>
-          <template #header>性别屏蔽</template>
-          <el-radio-group v-model="genderRadioVal">
-            <el-radio-button label="男性"></el-radio-button>
-            <el-radio-button label="女性"></el-radio-button>
-            <el-radio-button label="保密"></el-radio-button>
-            <el-radio-button label="不处理"></el-radio-button>
-          </el-radio-group>
         </el-card>
       </div>`,
     data() {
