b74946ed688522ec68c7fb5869a837e024dfd9ad
寒鸽子 <2978778354@qq.com>
2025-04-06 22:05:22 +0800

    refactor(layout): 优化 devTools 输入值处理逻辑 - 支持直接执行无参数的事件

--- src/layout/vue/panelSettingsVue.js
+++ src/layout/vue/panelSettingsVue.js
@@ -6,46 +6,46 @@ import gmUtil from "../../utils/gmUtil.js";
 export const panel_settings_vue = {
     template: `
       <div>
-      <el-card shadow="never">
-        <template #header>
-          <span>颜色设置</span>
-        </template>
-        <div class="el-horizontal-center">
-          选择器
-          <el-color-picker v-model="input_color"/>
-        </div>
-        <el-button @click="setBorderColorBut">设置边框色</el-button>
-        <el-button @click="setDefFontColorForOutputInformationBut">设置输出信息默认字体色</el-button>
-        <el-button @click="setTheFontColorForOutputInformationBut">设置输出信息高亮字体色</el-button>
-        <el-tooltip content="刷新页面生效">
-          <el-button @click="setDefInfoBut">恢复默认</el-button>
-        </el-tooltip>
-      </el-card>
-      <el-card shadow="never">
-        <template #header>
-          <span>页面右侧悬浮按钮设置</span>
-        </template>
-        <el-switch v-model="showRightTopMainButSwitch" active-text="显示按钮"></el-switch>
-        <el-tooltip content="页面每次加载完之后是否完整展示按钮，否则半隐藏">
-          <el-switch v-model="isFirstFullDisplay" active-text="初次完整显示"></el-switch>
-        </el-tooltip>
-        <el-tooltip content="页面每次加载完之后如完整展示按钮时，间隔10秒后半隐藏处理">
-          <el-switch v-model="isHalfHiddenIntervalAfterInitialDisplay"
-                     active-text="初次显示后间隔半隐藏"/>
-        </el-tooltip>
-      </el-card>
-      <el-card shadow="never">
-        <template #header>
-          <span>说明</span>
-        </template>
-        <div>按键盘tab键上的~键为展开关闭主面板</div>
-      </el-card>
-      <el-card shadow="never">
-        <template #header>
-          <span>devTools</span>
-        </template>
-        <el-input v-model.trim="devToolsInputVal" @keyup.enter.native="changeDevToolsInput"></el-input>
-      </el-card>
+        <el-card shadow="never">
+          <template #header>
+            <span>颜色设置</span>
+          </template>
+          <div class="el-horizontal-center">
+            选择器
+            <el-color-picker v-model="input_color"/>
+          </div>
+          <el-button @click="setBorderColorBut">设置边框色</el-button>
+          <el-button @click="setDefFontColorForOutputInformationBut">设置输出信息默认字体色</el-button>
+          <el-button @click="setTheFontColorForOutputInformationBut">设置输出信息高亮字体色</el-button>
+          <el-tooltip content="刷新页面生效">
+            <el-button @click="setDefInfoBut">恢复默认</el-button>
+          </el-tooltip>
+        </el-card>
+        <el-card shadow="never">
+          <template #header>
+            <span>页面右侧悬浮按钮设置</span>
+          </template>
+          <el-switch v-model="showRightTopMainButSwitch" active-text="显示按钮"></el-switch>
+          <el-tooltip content="页面每次加载完之后是否完整展示按钮，否则半隐藏">
+            <el-switch v-model="isFirstFullDisplay" active-text="初次完整显示"></el-switch>
+          </el-tooltip>
+          <el-tooltip content="页面每次加载完之后如完整展示按钮时，间隔10秒后半隐藏处理">
+            <el-switch v-model="isHalfHiddenIntervalAfterInitialDisplay"
+                       active-text="初次显示后间隔半隐藏"/>
+          </el-tooltip>
+        </el-card>
+        <el-card shadow="never">
+          <template #header>
+            <span>说明</span>
+          </template>
+          <div>按键盘tab键上的~键为展开关闭主面板</div>
+        </el-card>
+        <el-card shadow="never">
+          <template #header>
+            <span>devTools</span>
+          </template>
+          <el-input v-model.trim="devToolsInputVal" @keyup.enter.native="changeDevToolsInput"></el-input>
+        </el-card>
       </div>`,
     data() {
         return {
@@ -80,16 +80,20 @@ export const panel_settings_vue = {
             this.$alert("已恢复默认颜色，刷新生效");
         },
         changeDevToolsInput() {
-            if (this.devToolsInputVal === 'show-dev') {
+            const toolsInputVal = this.devToolsInputVal;
+            if (toolsInputVal === 'show-dev') {
                 setOpenDev(true)
                 eventEmitter.send('debugger-dev-show', true)
                 this.devToolsInputVal = ''
+                return
             }
-            if (this.devToolsInputVal === 'stop-dev') {
+            if (toolsInputVal === 'stop-dev') {
                 setOpenDev(false)
                 eventEmitter.send('debugger-dev-show', false)
                 this.devToolsInputVal = ''
+                return;
             }
+            eventEmitter.send(toolsInputVal)
         }
     },
     watch: {
