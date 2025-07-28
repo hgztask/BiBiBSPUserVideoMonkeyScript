d7b82fba4753ad35f422d7eb9278868536ff5998
寒鸽子 <2978778354@qq.com>
2025-07-29 00:34:23 +0800

    build: 添加性能测试相关配置并优化打包流程
    - 在 package.json 中添加了 build:perf 和 build:no-perf 脚本- 在 rollup.config.js 中增加了性能监控配置选项
    - 引入了 esbuild、vue 和 serve 等 rollup 插件，提升打包效率和功能
    - 新增 rollup-test-plugin.js插件，用于清除注释和优化代码格式

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,52 @@
 import importContent from 'rollup-plugin-import-content'
+import vue from 'rollup-plugin-vue';
+import esbuild from 'rollup-plugin-esbuild';
+import serve from 'rollup-plugin-serve'
+import test_plugin from './plugin/rollup-test-plugin.js'
 
 export default {
+    // 性能监控
+    perf: false,
     input: 'src/web/main.js',
     external: ['vue', 'dexie'],
     plugins: [
+        esbuild({
+            // 核心配置
+            target: 'es2020',
+            charset: 'utf8', // 明确使用 UTF-8 编码
+            // 生产环境优化
+            minify: false,
+            // none不保留注释，inline注释
+            legalComments: 'inline',
+        }),
         importContent({
             fileName: ['.css']
         }),
-        // terser({
-        //     compress: {
-        //         drop_console: false, // 不删除 console.log 语句
-        //         drop_debugger: false // 不删除 debugger 语句
-        //     }
-        // })
+        vue({
+            css: true,
+            compileTemplate: true // 编译模板
+        }),
+        test_plugin({
+            clearComments: true
+        }),
+        serve({
+            open: false,
+            port: 3000,
+            contentBase: 'dist',
+        }),
+        /*        terser({
+                    compress: {
+                        drop_console: false, // 不删除 console.log 语句
+                        drop_debugger: false // 不删除 debugger 语句
+                    }
+                })*/
     ],
     output: {
         file: 'dist/local_build.js',
         format: 'iife',
+        //hidden为隐藏 source map，inline为内联 source map，separate为外部 source map
+        // sourcemap: "inline",
+        compact: false,// 压缩代码
         globals: {
             vue: "Vue", // 这里指定 'vue' 模块对应的全局变量名为 'Vue'
             dexie: 'Dexie'
