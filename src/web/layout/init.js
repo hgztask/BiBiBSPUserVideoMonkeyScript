a680c9dff9bad32a8d236d55044209222812f63d
寒鸽子 <2978778354@qq.com>
2025-08-27 23:03:35 +0800

    perf(layout): 优化 element-ui样式插入加载时机
    - 将样式加载逻辑移至 DOMContentLoaded 事件内部

--- src/web/layout/init.js
+++ src/web/layout/init.js
@@ -5,18 +5,17 @@ import {addGzStyle, initVueApp} from "../utils/defUtil.js";
 import App from "./App.vue";
 import elUtil from "../utils/elUtil.js";
 
-if (document.head.querySelector('#element-ui-css') === null) {
-    const linkElement = document.createElement('link');
-    linkElement.rel = 'stylesheet';
-    linkElement.href = 'https://unpkg.com/element-ui@2.15.14/lib/theme-chalk/index.css'
-    linkElement.id = 'element-ui-css'
-    document.head.appendChild(linkElement)
-    linkElement.addEventListener('load', () => {
-        console.log('element-ui样式加载完成')
-    })
-}
-
 window.addEventListener('DOMContentLoaded', () => {
+    if (document.head.querySelector('#element-ui-css') === null) {
+        const linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = 'https://unpkg.com/element-ui@2.15.14/lib/theme-chalk/index.css'
+        linkElement.id = 'element-ui-css'
+        document.head.appendChild(linkElement)
+        linkElement.addEventListener('load', () => {
+            console.log('element-ui样式加载完成')
+        })
+    }
     const mainLayoutEl = document.createElement('div');
     document.body.appendChild(mainLayoutEl)
     window.mk_vue_app = initVueApp(mainLayoutEl, App);
