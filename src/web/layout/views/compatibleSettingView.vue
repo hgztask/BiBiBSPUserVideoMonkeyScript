d7ebeefb4ac8508ec95b83a3503b3bb7d11a9ede
寒鸽子 <2978778354@qq.com>
2025-12-31 01:30:01 +0800

    docs(setting): 更新Bewly插件兼容性说明
    - 移除了Bewly插件标题中的逗号
    - 更新了BewlyCat版本兼容性描述，改为可查看使用文档获取兼容版本信息

--- src/web/layout/views/compatibleSettingView.vue
+++ src/web/layout/views/compatibleSettingView.vue
@@ -45,9 +45,9 @@ export default {
       <el-switch v-model="bGateClearListNonVideoV" active-text="清空列表非视频元素" title="刷新页面后生效"/>
     </el-card>
     <el-card>
-      <template #header>Bewly插件(BewlyCat),原BewlyBewly</template>
+      <template #header>Bewly插件(BewlyCat)原BewlyBewly</template>
       <div>
-        仅兼容BewlyCat_1.5.2版本，不往前兼容
+        兼容BewlyCat的版本可看使用文档，不往前兼容
       </div>
       <el-switch v-model="compatible_BEWLY_BEWLY" active-text="首页适配"/>
     </el-card>
