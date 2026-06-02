b01d02e5c8f4c2db39a3605bf2e53ab3678e6d15
byhgz <byhgz@qq.com>
2026-06-03 02:53:58 +0800

    feat(ruleManagement): 补充粉丝牌名称修改功能
    - 新增 setFansNameBut 方法实现粉丝牌名称修改弹窗
    - 添加输入验证逻辑确保粉丝牌名称长度不超过6个字符
    - 在表格操作列中新增修改粉丝牌按钮
    - 调整布局栅格从 6:18 为 8:16 以适应新增功能
    - 集成 ElMessage 和 ElPrompt 组件进行用户交互反馈

--- src/web/layout/views/ruleManagement/FanCardLevelRestrictionsView.vue
+++ src/web/layout/views/ruleManagement/FanCardLevelRestrictionsView.vue
@@ -45,6 +45,27 @@ export default defineComponent({
         this.dataList = this.dataList.filter(item => item !== row)
         this.$notify({message: "已删除", position: "bottom-right"})
       })
+    },
+    setFansNameBut(row) {
+      this.$prompt("请输入新的粉丝牌名称", {
+        title: "修改粉丝牌名称",
+        inputValue: row.name,
+      }).then(({value}) => {
+        if (value === undefined || value.trim() === '') {
+          this.$message.warning("内容不可为空")
+          return this.setFansNameBut(row)
+        }
+        value = value.trim()
+        if (value.length > 6) {
+          this.$message.warning("请输入正确的粉丝牌名称")
+          return this.setFansNameBut(row)
+        }
+        if (this.dataList.some(item => item.name === value)) {
+          this.$message.warning("已存在该粉丝牌名称")
+          return this.setFansNameBut(row)
+        }
+        row.name = value
+      })
     }
   },
   created() {
@@ -56,7 +77,7 @@ export default defineComponent({
 <template>
   <div>
     <el-row>
-      <el-col :span="6">
+      <el-col :span="8">
         <el-table :data="dataList" border stripe>
           <el-table-column label="粉丝牌" prop="name" width="170"/>
           <el-table-column label="限制等级" width="155">
@@ -75,12 +96,13 @@ export default defineComponent({
             <template #default="scope">
               <gz-space>
                 <el-button type="warning" @click="delBut(scope.row)">移除</el-button>
+                <el-button @click="setFansNameBut(scope.row)">修改粉丝牌</el-button>
               </gz-space>
             </template>
           </el-table-column>
         </el-table>
       </el-col>
-      <el-col :span="18">
+      <el-col :span="16">
         <el-card>
           <template #header>
             <gz-space>
